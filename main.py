a162a23588a626d03c405566727103622de7eac0
Kevin <kevin.m.perilla@gmail.com>
2026-04-08 13:45:21 -0400

    Rework metrics controller

--- main.py
+++ main.py
@@ -60,24 +60,98 @@ def select_mode(key, mode):
     return mode
 
 
-def process_countdown(countdown_start, countdown_duration, frame):
-    if countdown_start is not None:
-        elapsed_in_countdown = time.time() - countdown_start
-        remaining_in_countdown = countdown_duration - elapsed_in_countdown
-        if remaining_in_countdown > 0:
-            number = int(remaining_in_countdown) + 1  # Displays 3, 2, 1
-            cv2.putText(
-                frame,
-                str(number),
-                (frame.shape[1] // 2 - 30, frame.shape[0] // 2),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                5,
-                (0, 255, 0),
-                8,
-            )
-        else:
-            countdown_start = None  # countdown finished, deinitialize
-    return countdown_start
+class MetricsController:
+    def __init__(self):
+        self.countdown_start = None
+        self.metrics_start = None
+        self.countdown_duration = 3
+        self.metrics_queue_ix = 0
+        self.metrics_results_dict = {}
+        self.total_frames_count = 0
+        self.valid_landmarks_count = 0
+        self.true_positive_classification_count = 0
+
+    def process_countdown(self, frame):
+        if self.countdown_start is not None:
+            elapsed_in_countdown = time.time() - self.countdown_start
+            remaining_in_countdown = self.countdown_duration - elapsed_in_countdown
+            if remaining_in_countdown > 0:
+                number = int(remaining_in_countdown) + 1  # Displays 3, 2, 1
+                cv2.putText(
+                    frame,
+                    str(number),
+                    (frame.shape[1] // 2 - 30, frame.shape[0] // 2),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    5,
+                    (0, 255, 0),
+                    8,
+                )
+            else:
+                self.countdown_start = None  # countdown finished, deinitialize
+
+    def start_countdown(self):
+        self.countdown_start = time.time()
+        self.metrics_queue_ix = 0
+
+    def process_frame(self, mode, frame):
+        if mode == 1 and not self.countdown_start and not self.metrics_start:
+            self.metrics_start = time.time()
+        if self.metrics_start is not None:
+            elapsed_in_metrics_countdown = time.time() - self.metrics_start
+            if elapsed_in_metrics_countdown < 8:
+                number = int(elapsed_in_metrics_countdown)
+                cv2.putText(
+                    frame,
+                    str(number),
+                    (frame.shape[1] * 3 // 4 - 30, frame.shape[0] * 3 // 4),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    5,
+                    (0, 255, 0),
+                    8,
+                )
+            else:
+                elapsed_in_metrics_countdown = 0
+                self.metrics_start = None
+                self.countdown_start = time.time()
+                valid_landmark_ratio = self.valid_landmarks_count / self.total_frames_count
+                true_positive_classification_ratio = self.true_positive_classification_count / self.total_frames_count
+                print('total_frames', self.total_frames_count)
+                print('TP', self.true_positive_classification_count)
+                self.metrics_results_dict[self.metrics_queue_ix] = {
+                    'label': METRICS_QUEUE[self.metrics_queue_ix],
+                    'valid_landmarks_ratio': valid_landmark_ratio,
+                    'true_positive_classification_ratio': true_positive_classification_ratio,
+                }
+                print(self.metrics_results_dict[self.metrics_queue_ix])
+                print()
+                total_frames_count = 0
+                valid_landmarks_count = 0
+                true_positive_classification_count = 0
+                self.metrics_queue_ix += 1
+                if self.metrics_queue_ix >= len(METRICS_QUEUE):
+                    mode = 0
+                    self.metrics_queue_ix = 0
+                    self.display_final_metrics()
+                else:
+                    print(self.metrics_queue_ix, METRICS_QUEUE[self.metrics_queue_ix])
+        return mode
+
+    def update_true_positive_rate(self, classification):
+        if self.metrics_start:
+            self.valid_landmarks_count += 1
+            if classification == METRICS_QUEUE[self.metrics_queue_ix]:
+                self.true_positive_classification_count += 1
+
+    def update_frame_count(self):
+            if self.metrics_start:
+                self.total_frames_count += 1
+    def display_final_metrics(self):
+        percent_sum = 0
+        for row in self.metrics_results_dict.values():
+            print(f'{row['label']}: {row['true_positive_classification_ratio']}')
+            percent_sum += row['true_positive_classification_ratio']
+        print()
+        print(f'Average: {percent_sum/len(self.metrics_results_dict)}')
 
 def main():
     # Load model
@@ -99,14 +173,7 @@ def main():
 
     detector = vision.HandLandmarker.create_from_options(options)
 
-    countdown_start = None
-    metrics_start = None
-    countdown_duration = 3
-    metrics_queue_ix = 0
-    metrics_results_dict = {}
-    total_frames_count = 0
-    valid_landmarks_count = 0
-    true_positive_classification_count = 0
+    metrics = MetricsController()
 
     # Camera
     cap = cv2.VideoCapture(0)
@@ -119,8 +186,7 @@ def main():
             break
         # Start timer for metrics
         if key & 0xFF == ord('m'):
-            countdown_start = time.time()
-            metrics_queue_ix = 0
+            metrics.start_countdown()
 
         # "n" is "normal mode" (0), "m" is "metrics mode" (1)
         mode = select_mode(key, mode)
@@ -129,48 +195,10 @@ def main():
 
         frame = cv2.flip(frame, 1)
 
-        countdown_start = process_countdown(countdown_start, countdown_duration, frame)
+        metrics.process_countdown(frame)
 
         # Start tracking metrics
-        if mode == 1 and not countdown_start and not metrics_start:
-            metrics_start = time.time()
-        if metrics_start is not None:
-            elapsed_in_metrics_countdown = time.time() - metrics_start
-            if elapsed_in_metrics_countdown < 8:
-                number = int(elapsed_in_metrics_countdown)
-                cv2.putText(
-                    frame,
-                    str(number),
-                    (frame.shape[1] * 3 // 4 - 30, frame.shape[0] * 3 // 4),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    5,
-                    (0, 255, 0),
-                    8,
-                )
-            else:
-                elapsed_in_metrics_countdown = 0
-                metrics_start = None
-                countdown_start = time.time()
-                valid_landmark_ratio = valid_landmarks_count / total_frames_count
-                true_positive_classification_ratio = true_positive_classification_count / total_frames_count
-                print('total_frames', total_frames_count)
-                print('TP', true_positive_classification_count)
-                metrics_results_dict[metrics_queue_ix] = {
-                    'label': METRICS_QUEUE[metrics_queue_ix],
-                    'valid_landmarks_ratio': valid_landmark_ratio,
-                    'true_positive_classification_ratio': true_positive_classification_ratio,
-                }
-                print(metrics_results_dict[metrics_queue_ix])
-                print()
-                total_frames_count = 0
-                valid_landmarks_count = 0
-                true_positive_classification_count = 0
-                metrics_queue_ix += 1
-                if metrics_queue_ix >= len(METRICS_QUEUE):
-                    mode = 0
-                    metrics_queue_ix = 0
-                else:
-                    print(metrics_queue_ix, METRICS_QUEUE[metrics_queue_ix])
+        mode = metrics.process_frame(mode, frame)
 
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -183,8 +211,8 @@ def main():
         timestamp += 1
 
         label_text = "No hand"
-        if metrics_start:
-            total_frames_count += 1
+
+        metrics.update_frame_count()
 
         if results.hand_landmarks:
             features = extract_features(results).reshape(1, -1)
@@ -199,10 +227,7 @@ def main():
             else:
                 label_text = "Low Confidence"
 
-            if metrics_start:
-                valid_landmarks_count += 1
-                if classification == METRICS_QUEUE[metrics_queue_ix]:
-                    true_positive_classification_count += 1
+            metrics.update_true_positive_rate(classification)
 
         cv2.putText(frame, label_text, (20, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
